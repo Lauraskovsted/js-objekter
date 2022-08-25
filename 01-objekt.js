ea0c1e8132cea07acfa8b09d3fecd54f613d8bee
Laura <laur730t@stud.kea.dk>
2022-08-25 10:03:45 +0200

    01-objekt øvelse done

---
+++ 01-objekt.js
@@ -0,0 +1,8 @@
+let dyr = {
+  navn: "gris",
+  type: "pattedyr",
+  billede: "http://mabe-kea.dk/E19/pics/pig.png",
+  levested: "svinefarm",
+};
+
+console.log(`navn:${dyr.navn} type:${dyr.type} levested:${dyr.levested}`);
